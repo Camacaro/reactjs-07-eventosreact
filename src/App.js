5d81913afc27d866a9843387ba7ecf7ed0d667ba
jcamacaro <jesus.alejandr@gmail.com>
2019-08-29 16:29:50 -0430

    agregando contenido

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
+import Eventos from './componentes/Eventos';
+
+class App extends Component {
+
+	token = '7XJ6RDWOUTKKZHGM7ACJ';
+
+	ordernar = 'date';
+
+	state = {
+		categorias: [],
+		eventos: []
+	}
+
+	/**
+	 * Al se ejecuta al cargar el componente
+	 */
+	componentDidMount() {
+		this.obtenerCategorias();
+	}
+
+	/**
+	 * Utilizando el async await en REACT
+	 */
+	obtenerCategorias = async () => {
+
+		let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`; 
+
+		await fetch(url)
+			.then( respuesta => {
+				return respuesta.json();
+			})
+			.then(categorias => {
+				this.setState({
+					categorias: categorias.categories
+				});
+			})
+	}
+
+	obtenerEventos = async (busqueda) => {
+
+		let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this.ordernar}&token=${this.token}&locale=es_ES`; 
+
+		await fetch(url)
+			.then( respuesta => {
+				return respuesta.json();
+			})
+			.then(eventos => {
+				this.setState({
+					eventos: eventos.events
+				});
+			})
+		
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Header/>
+
+				<div className="uk-container">
+					<Formulario 
+						categorias={this.state.categorias}
+						obtenerEventos= {this.obtenerEventos}
+					/>
+
+					<Eventos
+						eventos={this.state.eventos}
+					/>
+				</div>
+
+			</div>
+		);
+	}
+}
+
+export default App;
+
